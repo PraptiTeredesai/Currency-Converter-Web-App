c856b18c13cc409b45110ea2417523bfee489309
PraptiTeredesai <pariteredesai@gmail.com>
2024-10-07 12:29:25 +0530

    Add files via upload

---
+++ Currency_convertor/check.js
@@ -0,0 +1,19 @@
+// const fetch = require('node-fetch'); // Install using npm install node-fetch
+
+// async function checkCurrencyAPI() {
+//   const API_KEY = "YOUR-API-KEY"; // Replace with your API key
+//   const BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
+  
+//   try {
+//     let response = await fetch(BASE_URL);
+//     if (!response.ok) {
+//       throw new Error("API request failed with status: " + response.status);
+//     }
+//     let data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.error("Error fetching data: ", error);
+//   }
+// }
+
+// checkCurrencyAPI();
